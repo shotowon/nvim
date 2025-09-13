df29c0b2f01d95f08f683a3cf3e0275c303c3bea
Shoto <sanzhar.maratov@proton.me>
2025-09-13 15:14:43 +0000

    13.09.2025
    fix rust-analyzer lsp

--- lua/config/lsps/rust-analyzer.lua
+++ lua/config/lsps/rust-analyzer.lua
@@ -1,4 +1,10 @@
 require("lspconfig").rust_analyzer.setup {
-	capabilites = require('blink.cmp').get_lsp_capabilities(),
-	all_features = true,
+	capabilities = require('blink.cmp').get_lsp_capabilities(),
+	settings = {
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+		},
+	},
 }
